5860b5175966080ba13c4064927030164bf3bc82
Alejandro Arcucci <ale_arc2015@hotmail.com>
2019-09-30 20:20:30 -0300

    BIG CHUNGUS

--- Tp2/Tp2/Vistas/VentanaPersonal.xaml.cs
+++ Tp2/Tp2/Vistas/VentanaPersonal.xaml.cs
@@ -27,7 +27,7 @@ namespace Tp2
         {
             InitializeComponent();
             //Tomo como fuente del listbox la lista que entra
-            listBox.ItemsSource = personal;
+            lbxempleados.ItemsSource = personal;
         }
         //Armo un getter de personal
         public Personal GetPersonal()
@@ -38,10 +38,10 @@ namespace Tp2
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //Controlo vagamente que los campos no sean nulos o espacios en blanco
-            if (!string.IsNullOrWhiteSpace(textBox.Text) && !string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text) && !string.IsNullOrWhiteSpace(date.Text))
+            if (!string.IsNullOrWhiteSpace(txbnombre.Text) && !string.IsNullOrWhiteSpace(txbapellido.Text) && !string.IsNullOrWhiteSpace(txbdni.Text) && !string.IsNullOrWhiteSpace(dtpfecnac.Text) && !string.IsNullOrWhiteSpace(dtpfecalta.Text) && !string.IsNullOrWhiteSpace(txbsueldo.Text))
             {
                 //Armo el alumno con el constructor de la clase
-                empleado = new Personal(textBox.Text, textBox1.Text, .DisplayDate, textBox2.Text,, Convert.ToSingle(textBox3.Text));
+                empleado = new Personal(txbnombre.Text, txbapellido.Text, dtpfecnac.DisplayDate, txbdni.Text, dtpfecalta.DisplayDate, Convert.ToSingle(txbsueldo.Text));
                 //Cierro la ventana
                 this.Close();
             }
