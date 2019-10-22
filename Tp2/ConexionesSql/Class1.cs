78689fabd68bcdf0f7e10f76b9b41de8a57fcb96
Alejandro <ale_arc2015@hotmail.com>
2019-10-22 20:35:11 -0300

    DatabaseOn-ConDes

---
+++ Tp2/ConexionesSql/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConexionesSql
+{
+    public class Conexiones
+    {
+        public void Conectar()
+        {
+            string ConLine = @"server=DESKTOP-1822RQS;database=Instituto;integrated security=true";
+            SqlConnection connection = new SqlConnection(ConLine);
+            connection.Open();
+        }
+        public void Desconectar(SqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
